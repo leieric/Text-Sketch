a9e3c8ce23e59b3a64d5ab26f2bace5e72742002
Eric Lei <elei@seas.upenn.edu>
2024-01-22 09:09:34 -0500

    comments in train_sketch.sh

--- scripts/train_sketch.sh
+++ scripts/train_sketch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# lambda of 0.5 targets bpp of 0.015. lambda of 1.0 targets bpp of around 0.025.
-CUDA_VISIBLE_DEVICES=0, python train_compressai.py --dist_metric "ms-ssim" --dataset "/home/eric/data/CLIC/2021/hed" --batch-size 4 --save --cuda --lambda 0.5 --epochs 300
+# lambda of 0.5 targets bpp of 0.015. lambda of 1.0 targets bpp of around 0.025. 
+# Adjust TRAIN_DIR to where the training sketches are found (assumes CompressAI ImageFolder layout.)
+CUDA_VISIBLE_DEVICES=0, python train_compressai.py --dist_metric "ms-ssim" --dataset {TRAIN_DIR} --batch-size 4 --save --cuda --lambda 0.5 --epochs 300
