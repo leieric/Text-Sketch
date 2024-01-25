948d1d07ec2d115edf9a5cefa59b2c284e9eed3b
Eric Lei <elei@seas.upenn.edu>
2024-01-25 10:15:48 -0500

    script for generating/saving sketch images

--- generate_sketches.py
+++ generate_sketches.py
@@ -1,5 +1,22 @@
 '''This script takes in a data set of images
-and generates corresponding sketches'''
+and generates corresponding sketches. 
+
+Assume data_root and save_dir both match structure of CompressAI ImageFolder:
+https://interdigitalinc.github.io/CompressAI/datasets.html#imagefolder.
+
+For the save directory:  
+    - /save_dir/sketch_type/ 
+        - train/
+            - sketch_type_0.png
+            - sketch_type_1.png
+        - test/
+            - sketch_type_0.png
+            - sketch_type_1.png
+        - valid/
+            - sketch_type_0.png
+            - sketch_type_1.png
+
+For more info on input data_root structure, see dataloaders.py'''
 
 # import libraries
 from annotator.hed import HEDdetector
@@ -10,27 +27,57 @@ import dataloaders
 from argparse import ArgumentParser
 import tqdm
 import numpy as np
+import sys
+import os
 
 
 def main():
     # parse command-line arguments
     parser = ArgumentParser()
-    parser.add_argument('--sketch_type', default='segmentation', type=str)
+    parser.add_argument('--sketch_type', default='segmentation', type=str, 
+                        help='Supported sketch types: segmentation, hed.')
     parser.add_argument('--dataset', default='CLIC2021', type=str)
+    parser.add_argument('--split', default='train', type=str)
     parser.add_argument('--data_root', default='/home/eric/data/', type=str)
     parser.add_argument('--save_dir', default='/home/eric/data/CLIC/2021/', type=str)
-
     args = parser.parse_args()
+
+    # set save path
+    # example: '/home/eric/data/CLIC/2021/segmentation/train/'
+    save_path = os.path.join(args.save_dir, args.sketch_type, args.split)
 
     # load data
     dm = dataloaders.get_dataloader(args)
 
-    # iterate through each data split and generate/save sketches
-    for i, x in tqdm.tqdm(enumerate(dm.train_dset)):
+    # data split
+    if args.split == 'train':
+        data = dm.train_dset
+    elif args.split == 'test':
+        data = dm.test_dset
+    elif args.split == 'val' or args.split == 'valid':
+        data = dm.val_dset
+        args.split = 'valid'
+    else:
+        sys.exit('Not a valid split.')
+
+    # sketch generator function
+    if args.sketch_type == 'segmentation':
+        apply = UniformerDetector()
+    elif args.sketch_type == 'hed':
+        apply = HEDdetector()
+    else:
+        sys.exit("Not a valid sketch type. Choose 'segmentation' or 'hed'.")
+
+    # iterate through data and generate/save sketches
+    for i, x in tqdm.tqdm(enumerate(data)):
         x = x[0]
-        x_im = (255*x.permute(1,2,0)).numpy().astype(np.uint8)
-        im = resize_image(HWC3(x_im), 512)
-        apply_seg = UniformerDetector()
-        _, seg_map = apply_seg(im)
-        sketch = np.squeeze(seg_map)
-        sketch.save(f'{args.data_root}/CLIC/2021/segmentation/test/segmentation_{i}.png')
+        x_img = (255*x.permute(1,2,0)).numpy().astype(np.uint8)
+        img = resize_image(HWC3(x_img), 512)
+        sketch = apply(img)
+        sketch_img = Image.fromarray(sketch)
+        sketch_img.save(os.path.join(save_path, f'{args.sketch_type}_{i}.png'))
+    return
+
+
+if __name__ == "__main__":
+    main()
